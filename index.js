579f3e2221fcdb08414911dd95c006411dba1548
enaszaid995 <enaszaid995@gmail.com>
2022-10-28 15:49:12 -0700

    update

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.use((error , req ,res, next)=>{
 })
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vtlqonc.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
-    app.listen(5000);
+    app.listen(process.env.PORT||5000);
 }).catch(error => console.log(error));
